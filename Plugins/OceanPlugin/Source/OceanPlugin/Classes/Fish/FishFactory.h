0d7d981902b1619fde157a1c43c21cb91e80cb1c
unknown <kcbryant@FL222-15.admin.wpi.edu>
2015-03-31 13:42:03 -0400

    Added FishFactory class (empty currently) to use later

---
+++ Plugins/OceanPlugin/Source/OceanPlugin/Classes/Fish/FishFactory.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "OceanPluginPrivatePCH.h"
+#include "GameFramework/Actor.h"
+#include "FishFactory.generated.h"
+
+/**
+*
+*/
+UCLASS()
+class AFishFactory : public AActor
+{
+	GENERATED_BODY()
+
+};
---
+++ Plugins/OceanPlugin/Source/OceanPlugin/Private/Fish/FishFactory.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OceanPluginPrivatePCH.h"
+#include "Fish/FishFactory.h"
+#include "Fish/FishManager.h"
+#include "Fish/FlockFish.h"
